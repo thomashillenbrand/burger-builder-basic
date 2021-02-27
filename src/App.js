3e748eae6bf5abbc925ebcb39f038bb525f26d01
Thomas Hillenbrand <thomashillenbrand16@gmail.com>
2021-02-26 21:25:38 -0500

    Initial Project Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import classes from "./App.module.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className={classes.App}>
+        <h1>My Burger App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
